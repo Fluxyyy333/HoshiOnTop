b098224ca292714d3a828c7307e71c17012f8fc7
Fluxyyy333 <gdevan903@gmail.com>
2026-06-04 00:55:41 +0800

    Add Sell Lemon (v1.0.0) + register GameId 7395930870 in GameList

--- GameList.lua
+++ GameList.lua
@@ -14,4 +14,5 @@ return {
     [10004244222] = "https://raw.githubusercontent.com/Fluxyyy333/HoshiOnTop/main/kalb.lua",
     [10039338037] = "https://raw.githubusercontent.com/Fluxyyy333/HoshiOnTop/main/buildaringfarm.lua",
     [9792947201]  = "https://raw.githubusercontent.com/Fluxyyy333/HoshiOnTop/main/slimerng.lua",
+    [7395930870]  = "https://raw.githubusercontent.com/Fluxyyy333/HoshiOnTop/main/selllemon.lua",
 }
